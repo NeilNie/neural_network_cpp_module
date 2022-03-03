8103f66205a0dac9f982222b099520396541e50e
Neil Nie <yongyang.nie@gmail.com>
2022-03-02 23:09:19 -0500

    update implementation to include more test points

--- vector_vs_list/vector_vs_list.cpp
+++ vector_vs_list/vector_vs_list.cpp
@@ -3,6 +3,7 @@
 #include <random>
 #include <vector>
 #include <list>
+#include <set>
 #include <functional>
 #include <unordered_set>
 #include <concepts>
@@ -52,77 +53,103 @@ ContainerType insert_random_numbers_to_container(std::vector<int> numbers) {
                 break;
             }
         }
-        if (!inserted) {
+        if (!inserted)
             container.push_back(number);
-        }
     }
     return container;
+}
+
+template <class T>
+T insert_random_numbers_to_set(std::vector<int> numbers) {
+
+    T set;
+    for (int number: numbers)
+        set.insert(number);
+    return set;
 }
 
 template <class T>
 void time_method(T (*f)(std::vector<int>), const std::vector<int>& data) {
 
     auto start = std::chrono::high_resolution_clock::now();
-    auto list = f(data);
+    f(data);
     auto end = std::chrono::high_resolution_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " milliseconds" << "\n";
 
 }
 
-int main(int argc, char *argv[]) {
+int main() {
 
     auto vector_10k = generate_random_numbers(10'000);
     auto vector_20k = generate_random_numbers(20'000);
     auto vector_30k = generate_random_numbers(30'000);
     auto vector_40k = generate_random_numbers(40'000);
     auto vector_50k = generate_random_numbers(50'000);
-    auto vector_60k = generate_random_numbers(40'000);
-    auto vector_70k = generate_random_numbers(50'000);
+    auto vector_60k = generate_random_numbers(60'000);
+    auto vector_70k = generate_random_numbers(70'000);
+    auto vector_80k = generate_random_numbers(80'000);
+    auto vector_90k = generate_random_numbers(90'000);
 
     std::cout << "std::list -> input size: 10k" << "\n";
     time_method(&insert_random_numbers_to_container<std::list<int>>, vector_10k);
-
     std::cout << "std::list -> input size: 20k" << "\n";
     time_method(&insert_random_numbers_to_container<std::list<int>>, vector_20k);
-
     std::cout << "std::list -> input size: 30k" << "\n";
     time_method(&insert_random_numbers_to_container<std::list<int>>, vector_30k);
-
     std::cout << "std::list -> input size: 40k" << "\n";
     time_method(&insert_random_numbers_to_container<std::list<int>>, vector_40k);
-
     std::cout << "std::list -> input size: 50k" << "\n";
     time_method(&insert_random_numbers_to_container<std::list<int>>, vector_50k);
-
     std::cout << "std::list -> input size: 60k" << "\n";
     time_method(&insert_random_numbers_to_container<std::list<int>>, vector_60k);
-
     std::cout << "std::list -> input size: 70k" << "\n";
     time_method(&insert_random_numbers_to_container<std::list<int>>, vector_70k);
+    std::cout << "std::list -> input size: 80k" << "\n";
+    time_method(&insert_random_numbers_to_container<std::list<int>>, vector_80k);
+    std::cout << "std::list -> input size: 90k" << "\n";
+    time_method(&insert_random_numbers_to_container<std::list<int>>, vector_90k);
 
     std::cout << "\n" << "\n";
 
     std::cout << "std::vector -> input size: 10k" << "\n";
     time_method(&insert_random_numbers_to_container<std::vector<int>>, vector_10k);
-
     std::cout << "std::vector -> input size: 20k" << "\n";
     time_method(&insert_random_numbers_to_container<std::vector<int>>, vector_20k);
-
     std::cout << "std::vector -> input size: 30k" << "\n";
     time_method(&insert_random_numbers_to_container<std::vector<int>>, vector_30k);
-
     std::cout << "std::vector -> input size: 40k" << "\n";
     time_method(&insert_random_numbers_to_container<std::vector<int>>, vector_40k);
-
     std::cout << "std::vector -> input size: 50k" << "\n";
     time_method(&insert_random_numbers_to_container<std::vector<int>>, vector_50k);
-
     std::cout << "std::vector -> input size: 60k" << "\n";
     time_method(&insert_random_numbers_to_container<std::vector<int>>, vector_60k);
-
     std::cout << "std::vector -> input size: 70k" << "\n";
     time_method(&insert_random_numbers_to_container<std::vector<int>>, vector_70k);
+    std::cout << "std::vector -> input size: 80k" << "\n";
+    time_method(&insert_random_numbers_to_container<std::vector<int>>, vector_80k);
+    std::cout << "std::vector -> input size: 90k" << "\n";
+    time_method(&insert_random_numbers_to_container<std::vector<int>>, vector_90k);
+
+    std::cout << "\n" << "\n";
+
+    std::cout << "std::set-> input size: 10k" << "\n";
+    time_method(&insert_random_numbers_to_set<std::set<int>>, vector_10k);
+    std::cout << "std::set -> input size: 20k" << "\n";
+    time_method(&insert_random_numbers_to_set<std::set<int>>, vector_20k);
+    std::cout << "std::set -> input size: 30k" << "\n";
+    time_method(&insert_random_numbers_to_set<std::set<int>>, vector_30k);
+    std::cout << "std::set -> input size: 40k" << "\n";
+    time_method(&insert_random_numbers_to_set<std::set<int>>, vector_40k);
+    std::cout << "std::set -> input size: 50k" << "\n";
+    time_method(&insert_random_numbers_to_set<std::set<int>>, vector_50k);
+    std::cout << "std::set -> input size: 60k" << "\n";
+    time_method(&insert_random_numbers_to_set<std::set<int>>, vector_60k);
+    std::cout << "std::set -> input size: 70k" << "\n";
+    time_method(&insert_random_numbers_to_set<std::set<int>>, vector_70k);
+    std::cout << "std::set -> input size: 80k" << "\n";
+    time_method(&insert_random_numbers_to_set<std::set<int>>, vector_80k);
+    std::cout << "std::set -> input size: 90k" << "\n";
+    time_method(&insert_random_numbers_to_set<std::set<int>>, vector_90k);
 
     return 0;
-
 }
