15c373a634c72269c20dfaae32993ac108cca1ed
Neil Nie <yongyang.nie@gmail.com>
2022-04-28 18:48:03 -0400

    move matrix loading routine to mat class

--- src/module/Module.cpp
+++ src/module/Module.cpp
@@ -20,8 +20,11 @@ module LibNeuralNets;
 
 using namespace nn;
 
+static constexpr char bias_txt_suffix[] = "_bias.txt";
+static constexpr char weights_txt_suffix[] = "_weights.txt";
+
 template <const char *DataPathSuffix, bool Verbose = true>
-static void read_txt_data_to_matrix(const std::string& path,
+static inline void read_txt_data_to_matrix(const std::string& path,
                                     const std::string& data_path,
                                     nn::Mat<float>& out)
 {
@@ -31,54 +34,10 @@ static void read_txt_data_to_matrix(const std::string& path,
             new char[path.length() + data_path.length() + std::strlen(DataPathSuffix) + 1]);
     std::sprintf(full_path.get(), "%s%s%s", path.c_str(), data_path.c_str(), DataPathSuffix);
     if constexpr (Verbose)
-        std::cout << "Begin loading..." << "\n";
+    std::cout << "Begin loading..." << "\n";
 
-    if constexpr (Verbose)
-        std::cout << "loading from file: " << full_path.get() << "\n";
-
-    // using C stdio instead of std::fstream because the later seems to incur
-    // internal compiler error in a module
-    FILE *file = std::fopen(full_path.get(), "r");
-    if (file == nullptr)
-        throw std::system_error(errno, std::generic_category());
-    int count = 0;
-
-    int rows, cols;
-    std::vector<float> data;
-    char *line = nullptr;
-    size_t n = 0;
-    while (::getline(&line, &n, file) != -1) {
-        float line_data = std::strtof(line, nullptr);
-        if (count == 0) {
-            rows = (int) line_data;
-        } else if (count == 1) {
-            cols = (int) line_data;
-        } else {
-            data.push_back(line_data);
-        }
-        count++;
-    }
-    std::free(line);
-    bool ferr = std::ferror(file);
-    std::fclose(file);
-    if (ferr)
-        throw std::system_error(errno, std::generic_category());
-
-    if constexpr (Verbose)
-        std::cout << "matrix rows: " << rows << " columns: " << cols << "\n";
-
-    out.resize(rows, cols);
-
-    for (auto i = 0; i < out.n_rows(); i++)
-        for (auto j = 0; j < out.n_cols(); j++)
-            out(i, j) = data[i * out.n_cols() + j]; //
-
-    if constexpr (Verbose)
-        std::cout << "Finished loading" << "\n";
+    out.load_from_txt(full_path.get());
 }
-
-static constexpr char bias_txt_suffix[] = "_bias.txt";
-static constexpr char weights_txt_suffix[] = "_weights.txt";
 
 Module::Module(const std::string& model_path, const std::vector<std::string>& names)
 {
